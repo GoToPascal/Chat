3283128dba4dd780b62a0c7f36fa9c0b43fe7973
Oleg Popov <oleg25100130719@mail.ru>
2016-06-23 12:33:25 +0300

    faili

--- index.js
+++ index.js
@@ -2,8 +2,9 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var plcount= 0
-var playername= [10['AAAAD','anonim']]
-
+var i= 0
+var playername= [10['ID','Name']]
+var banned=[4[' ']]
 
 
 
@@ -14,24 +15,38 @@ app.get('/', function(req, res){
 io.on('connection', function(socket){
 	plcount=plcount+1
 	console.log('User conected. Users online: ',plcount);
-	
+		  //for (i=0,i<11,i++) 
+//{ if (playername[i[0]] == '')
+//	{playername[i[0]]=socket.id.toString().substr(socket.id.toString().length-5,socket.id.toString().length);
+//break}}	
+	//});
    socket.on('disconnect', function(){
    plcount=plcount-1
    console.log('User disconnected. Users online: ',plcount);
   });
   socket.on('chat message', function(msg){
-	  //for (i=0,i<11,i++) 
-	  //{ if (playername[i,] != 'anonim') }
-  
-  
-	  {io.emit('chat message',socket.id.toString().substr(socket.id.toString().length-5,socket.id.toString().length)+': '+msg);	}
-      //{io.emit('chat message',playername+': '+msg);	}
-	//if (playername == 'anonim') 
-		//{playername=msg}
+ 
+     // for (i=0,i<11,i++)
+	  //{ if (playername[i[0]] == socket.id.toString().substr(socket.id.toString().length-5),socket.id.toString().length)
+		 // {if (playername[i[1]]=='') {playername[i[1]]=msg; break;}  
+		  //else {io.emit('chat message',playername[i[1]],+': '+msg);	break;}
+		//  }
+	//  }		 
+	  if     (socket.name!=undefined)
+      { 
+         io.emit('chat message',socket.name+': '+msg);	
+		  }
+		  
+      else {
+		  for (i=0;i<4;i++){
+        if (msg==banned[i]){msg='Нарушитель'}
+		                   }
+	         socket.name=msg
+	        }
   });
 });
 
 
-http.listen(7070, function(){
-  console.log('Server online (port:7070)');
+http.listen(8080, function(){
+  console.log('Server online (port:8080)');
 });
